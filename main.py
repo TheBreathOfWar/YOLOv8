982c08ea980b35c8f3dfc10e8cdba52b2a375819
TanosIsAGod <tanosisagod@gmail.com>
2023-10-08 15:45:09 +0300

    Get image path

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import argparse
 
 if __name__ == "__main__":
+    pass
     parser = argparse.ArgumentParser()
     parser.add_argument("--image", required=True, help="Path to the input image")
     parser.add_argument("--selection-color", required=True, help="Color of masks and boxes")
     parser.add_argument("--selection-type", required=True, help="1 for boxes or 0 for masks")
     args = parser.parse_args()
+
+    source = args.image
