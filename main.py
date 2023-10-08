5f1bef4922eeff394a795ac42a01aa71eb76b006
TanosIsAGod <tanosisagod@gmail.com>
2023-10-08 15:55:58 +0300

    Draw masks and boxes on image

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import argparse
 from ultralytics import YOLO
-import cv2
+from utils import *
+from PIL import Image
 
 if __name__ == "__main__":
-    pass
     parser = argparse.ArgumentParser()
     parser.add_argument("--image", required=True, help="Path to the input image")
     parser.add_argument("--selection-color", required=True, help="Color of masks and boxes")
@@ -17,3 +17,22 @@ if __name__ == "__main__":
     image = cv2.imread(source)
     h, w, _ = image.shape
     results = model(source=image.copy(), classes=0, retina_masks=True, conf=0.2, verbose=False)
+
+    for r in results:
+        boxes = r.boxes
+        masks = r.masks
+        masks = masks.data.cpu()
+        for mask, box in zip(masks.data.cpu().numpy(), boxes):
+            if selection_type == 1:
+                x_min = int(box.data[0][0])
+                y_min = int(box.data[0][1])
+                x_max = int(box.data[0][2])
+                y_max = int(box.data[0][3])
+                draw_box(image, [x_min, y_min, x_max, y_max])
+            elif selection_type == 0:
+                mask = cv2.resize(mask, (w, h))
+                image = draw_mask(image, mask)
+            else:
+                print('Error: incorrect selection_type, expected 1 or 0')
+
+    image = Image.fromarray(image[..., ::-1])
