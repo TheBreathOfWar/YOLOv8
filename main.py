8ca5b00978c24ec80e23ed1867ab5973bfb364f6
TanosIsAGod <tanosisagod@gmail.com>
2023-10-08 16:40:09 +0300

    Fix argparse arguments

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ from PIL import ImageColor
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--image", required=True, help="Path to the input image")
-    parser.add_argument("--selection-color", required=True, help="Color in HEX format without # (FFFFFF)")
-    parser.add_argument("--selection-type", required=True, help="1 for boxes or 0 for masks")
+    parser.add_argument("--selection_color", required=True, help="Color in HEX format without # (FFFFFF)")
+    parser.add_argument("--selection_type", required=True, help="1 for boxes or 0 for masks")
     args = parser.parse_args()
 
     source = args.image
