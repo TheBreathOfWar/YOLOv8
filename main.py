a23cb65161092843fa3973ca8236c31a39fc9985
TanosIsAGod <tanosisagod@gmail.com>
2023-10-08 16:03:42 +0300

    Get image path

--- main.py
+++ main.py
@@ -6,3 +6,5 @@ if __name__ == "__main__":
     parser.add_argument("--selection-color", required=True, help="Color of masks and boxes")
     parser.add_argument("--selection-type", required=True, help="1 for boxes or 0 for masks")
     args = parser.parse_args()
+
+    source = args.image
