f2c73535056aebbc90bee04a5d4dd5a11e582ae1
TanosIsAGod <tanosisagod@gmail.com>
2023-10-08 16:39:05 +0300

    Set color from HEX to BGR

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import argparse
 from ultralytics import YOLO
 import cv2
+from PIL import ImageColor
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--image", required=True, help="Path to the input image")
-    parser.add_argument("--selection-color", required=True, help="Color of masks and boxes")
+    parser.add_argument("--selection-color", required=True, help="Color in HEX format without # (FFFFFF)")
     parser.add_argument("--selection-type", required=True, help="1 for boxes or 0 for masks")
     args = parser.parse_args()
 
@@ -17,3 +18,5 @@ if __name__ == "__main__":
     image = cv2.imread(source)
     h, w, _ = image.shape
     results = model(source=image.copy(), classes=0, retina_masks=True, conf=0.2, verbose=False)
+
+    color = list(ImageColor.getcolor('#'+color, "RGB"))[::-1]
