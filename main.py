9c66e7ed8fdc22893917dc5b7ca4460f6aa61aa4
TanosIsAGod <tanosisagod@gmail.com>
2023-10-08 17:07:00 +0300

    Add comments

--- main.py
+++ main.py
@@ -1,33 +1,46 @@
-import argparse
-from ultralytics import YOLO
-from PIL import ImageColor
-from utils import *
-from PIL import Image
+# Import necessary libraries
+import argparse  # For parsing command-line arguments
+from ultralytics import YOLO  # Import YOLO model from ultralytics
+from PIL import ImageColor  # For converting color from HEX to RGB
+from utils import *  # Import utility functions
+from PIL import Image  # For image processing
 
 if __name__ == "__main__":
+    # Parse command-line arguments
     parser = argparse.ArgumentParser()
     parser.add_argument("--image", required=True, help="Path to the input image")
     parser.add_argument("--selection_color", required=True, help="Color in HEX format without # (FFFFFF)")
     parser.add_argument("--selection_type", required=True, help="1 for boxes or 0 for masks")
     args = parser.parse_args()
 
+    # Extract arguments
     source = args.image
     selection_type = int(args.selection_type)
     color = args.selection_color
 
+    # Load YOLO model for object detection
     model = YOLO('yolov8x-seg.pt')
+
+    # Read the input image using OpenCV
     image = cv2.imread(source)
     h, w, _ = image.shape
+
+    # Perform object detection using YOLO
     results = model(source=image.copy(), classes=0, retina_masks=True, conf=0.2, verbose=False)
 
-    color = list(ImageColor.getcolor('#'+color, "RGB"))[::-1]
+    # Convert HEX color to BGR format
+    color = list(ImageColor.getcolor('#' + color, "RGB"))[::-1]
 
+    # Process the results
     for r in results:
         boxes = r.boxes
         masks = r.masks
         masks = masks.data.cpu()
+
+        # Iterate over detected objects
         for mask, box in zip(masks.data.cpu().numpy(), boxes):
             if selection_type == 1:
+                # Draw bounding boxes if selection_type is 1
                 x_min = int(box.data[0][0])
                 y_min = int(box.data[0][1])
                 x_max = int(box.data[0][2])
@@ -36,11 +49,15 @@ if __name__ == "__main__":
                 draw_box(image, [x_min, y_min, x_max, y_max], color)
 
             elif selection_type == 0:
+                # Draw masks if selection_type is 0
                 mask = cv2.resize(mask, (w, h))
                 image = draw_mask(image, mask, color)
             else:
                 print('Error: incorrect selection_type, expected 1 or 0')
 
+    # Convert the processed image back to PIL format
     image = Image.fromarray(image[..., ::-1])
+
+    # Save the result as an image
     image.save('output.jpg')
     print('Result saved to output.jpg')
--- utils.py
+++ utils.py
@@ -2,20 +2,37 @@ import cv2
 import numpy as np
 
 
+# Function to draw a bounding box around an object and label it
 def draw_box(image, box, color=[56, 56, 255], label='person'):
+    # Define corner points of the bounding box
     c1, c2 = (int(box[0]), int(box[1])), (int(box[2]), int(box[3]))
+
+    # Draw the bounding box on the image
     cv2.rectangle(image, c1, c2, color, thickness=3, lineType=cv2.LINE_AA)
 
+    # Calculate the size of the text label
     t_size = cv2.getTextSize(label, 0, fontScale=1, thickness=2)[0]
+
+    # Adjust the position for drawing the label background
     c2 = c1[0] + t_size[0], c1[1] - t_size[1] - 3
+
+    # Draw a filled rectangle as the label background
     cv2.rectangle(image, c1, c2, color, -1, cv2.LINE_AA)
+
+    # Draw the label text on the image
     cv2.putText(image, label, (c1[0], c1[1] - 2), 0, 1, [255, 255, 255], thickness=2, lineType=cv2.LINE_AA)
 
 
+# Function to draw a colored mask over an image
 def draw_mask(image, mask, color=[56, 56, 255], alpha=0.4):
+    # Create a colored mask from the input mask
     colored_mask = np.expand_dims(mask, 0).repeat(3, axis=0)
     colored_mask = np.moveaxis(colored_mask, 0, -1)
+
+    # Create a masked image overlay
     masked = np.ma.MaskedArray(image, mask=colored_mask, fill_value=color)
+
+    # Combine the original image with the masked overlay using alpha blending
     image_overlay = masked.filled()
     image_combined = cv2.addWeighted(image, 1 - alpha, image_overlay, alpha, 0)
 
